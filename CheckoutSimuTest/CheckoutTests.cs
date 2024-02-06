ddc7f3a0556afacdb337d66fa22f9e5ce435c2e2
Junaid <junaid2396@hotmail.com>
2024-02-06 18:03:08 +0000

    Added Tests for different use cases(Red)

---
+++ CheckoutSimuTest/CheckoutTests.cs
@@ -0,0 +1,52 @@
+namespace CheckoutSimuTest
+{
+    [TestFixture]
+    public class CheckoutTests
+    {
+        [Test]
+        public void GetTotalPrice_SingleItem_ReturnsCorrectPrice()
+        {
+            //Arrange
+            ICheckout checkout = new Checkout();
+
+            //Act
+            checkout.Scan("A");
+
+            //Assert 
+            Assert.That(checkout.GetTotalPrice(), Is.EqualTo(50));
+        }
+
+        [Test]
+        public void GetTotalPrice_MultipleItems_ReturnsCorrectPrice()
+        {
+            //Arrange
+            ICheckout checkout = new Checkout();
+
+            //Act
+            checkout.Scan("A");
+            checkout.Scan("B");
+            checkout.Scan("A");
+            checkout.Scan("A");
+            checkout.Scan("B");
+            checkout.Scan("A");
+
+            //Assert 
+            Assert.That(checkout.GetTotalPrice(), Is.EqualTo(225));
+        }
+
+        [Test]
+        public void GetTotalPrice_SpecialPrice_ReturnsCorrectPrice()
+        {
+            //Arrange
+            ICheckout checkout = new Checkout();
+
+            //Act
+            checkout.Scan("B");
+            checkout.Scan("A");
+            checkout.Scan("B");
+
+            //Assert 
+            Assert.That(checkout.GetTotalPrice(), Is.EqualTo(95));
+        }
+    }
+}
