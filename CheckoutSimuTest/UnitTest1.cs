8ad66e73e56c9b920809064236e1f9c9462a53cd
Junaid <junaid2396@hotmail.com>
2024-02-06 17:49:14 +0000

    Add project files.

---
+++ CheckoutSimuTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace CheckoutSimuTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
